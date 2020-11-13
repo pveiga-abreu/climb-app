e749a7f0fc10c766702f8956bcb6e12804ce9dbe
marcuscastilho <marcus@iconexlog.com.br>
2020-11-13 00:30:30 -0300

    adicionando nvoas funcionalidades

---
+++ front-end-web/src/components/Load/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Loader from 'react-loader-spinner'
+
+export const Load = () => (
+  <Loader className='load' type="BallTriangle" color="#314ba3" height={30} width={70}/>
+)
